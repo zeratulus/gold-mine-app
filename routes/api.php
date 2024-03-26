170b0428363a992b0a99d8dddf22dc5ada33fd34
Serhii Herenko <ailus665@gmail.com>
2024-03-26 19:49:11 +0200

    Added Country routes and generate route.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\CountryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::controller(CountryController::class)->group(function () {
+    Route::post('/countries', 'index');
+    Route::post('/country/{id}', 'store');
+    Route::get('/country/{id}', 'show');
+    Route::delete('/country/{id}', 'destroy');
+});
+
+Route::get('/generate', function () {
+    //TODO: Launch seeders here
+});
